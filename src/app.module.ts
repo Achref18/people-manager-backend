e29a44ec23da928d0faf341b9204bbeefb79a5c6
Achref <saidiachref70@gmail.com>
2024-06-20 12:01:26 +0100

    Import schema validation, mongoose  and Person module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PersonModule } from '@modules/person/person.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { environmentSchema } from 'config/env.schema';
+import { MongodbConfigService } from '@config/mongodb.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: environmentSchema,
+    }),
+    MongooseModule.forRootAsync({
+      useClass: MongodbConfigService,
+    }),
+    PersonModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
