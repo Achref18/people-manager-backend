b2de41b2c5d813ee68fa18899bdb0c9008df2059
Achref <saidiachref70@gmail.com>
2024-06-20 12:00:19 +0100

    Add global project configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { validationPipeExceptionFactory } from 'utils/format.validation-pipe';
+import { ConfigService } from '@nestjs/config';
+import { envConstants } from '@config/env.constant';
+import { splitString } from 'utils/splitString';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService = app.get(ConfigService);
+
+  app.enableCors({
+    origin: splitString(configService.get<string>(envConstants.ORIGIN), ','),
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      exceptionFactory: validationPipeExceptionFactory,
+      stopAtFirstError: false,
+    }),
+  );
+
+  await app.listen(configService.get<number>(envConstants.PORT) || 3000);
 }
 bootstrap();
